92f7cddb9e5ecdc6d63e54270c8294713940ef15
VanAnh179 <vananh9684@gmail.com>
2024-12-18 14:21:00 +0700

    xóa phần tử ngẫu nhiên + event onclick

--- Assets/Script/Grid.cs
+++ Assets/Script/Grid.cs
@@ -1,12 +1,14 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-public class Grid : Selectable
+using UnityEngine.EventSystems;
+public class Grid : Selectable, IPointerClickHandler
 {
-    public GameObject num_text;
-    private int num_ = 0;
+    public GameObject num_text; //text hiển thị số
+    [HideInInspector] public int num_ = 0; // giá trị số của ô (hiển thị trong Inspector)
+    public bool is_selected = false; //kiểm tra trạng thái chọn
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +21,11 @@ public class Grid : Selectable
     {
         
     }
+
+    // hiển thị số trên mỗi obj
     public void DisplayText()
     {
-        if(num_ <= 0)
+        if(num_ <= 0) // bằng 0 thì xóa
         {
             num_text.GetComponent<TextMeshProUGUI>().text = " ";
             
@@ -32,9 +36,24 @@ public class Grid : Selectable
         }
         
     }
+    // gán giá trị số cho obj và hiển thị
     public void SetNum(int number)
     {
         num_ = number;
         DisplayText();
+    }
+
+    // xử lý khi nhấp vào ô
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // bật trạng thái "được chọn"
+        is_selected = true;
+        Debug.Log($"square is selected: {num_}"); //hiển thị thông tin debug (ô được chọn + giá trị tương ứng)
+
+        // hiển thị số của ô đã chọn trong Inspector
+        FindObjectOfType<SudokuGrid>().DisplaySelectedValue(num_);
+
+        // đổi màu nền của ô để hiển thị rằng nó đã được chọn
+        GetComponent<Image>().color = Color.yellow;
     }
 }
--- Assets/Script/SudokuGrid.cs
+++ Assets/Script/SudokuGrid.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -14,6 +14,7 @@ public class SudokuGrid : MonoBehaviour
 
     private List<GameObject> grid_squares_ = new List<GameObject>();
     private int selected_grid_data = -1;
+    public int selectedValue = 0; // Biến lưu giá trị của ô được chọn
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -73,6 +74,8 @@ public class SudokuGrid : MonoBehaviour
             col_num++;
         }
     }
+
+    //chọn ngẫu nhiên một bản đồ Sudoku từ danh sách được lưu trữ trong SudokuData
     private void SetGridNum(string map)
     {
         selected_grid_data = Random.Range(0, SudokuData.instance.sudoku_game[map].Count);
@@ -81,20 +84,56 @@ public class SudokuGrid : MonoBehaviour
         SetGridData(data);
     }
 
+    // xóa vị trí ngẫu nhiên trên matrix từ SudokuData rồi mới truyền xuống obj
+    private List<List<int>> RemoveRandomItems(List<List<int>> matrix, int gridRemove)
+    {
+        List<(int, int)> positions = new List<(int, int)>();
+
+        // thu thập tất cả các vị trí trong ma trận
+        for (int i = 0; i < matrix.Count; i++)
+        {
+            for (int j = 0; j < matrix[i].Count; j++)
+            {
+                positions.Add((i, j));
+            }
+        }
+
+        System.Random random = new System.Random();
+
+        // tóa ngẫu nhiên `gridRemove` vị trí
+        for (int i = 0; i < gridRemove && positions.Count > 0; i++)
+        {
+            int indexToRemove = random.Next(positions.Count);
+            var (row, col) = positions[indexToRemove];
+            matrix[row][col] = 0; // Gán giá trị tại vị trí đó là 0
+            positions.RemoveAt(indexToRemove); // Loại bỏ vị trí đã xóa khỏi danh sách
+        }
+
+        return matrix;
+    }
+
+    
     private void SetGridData(SudokuData.SudokuBoard data)
     {
-        //for (int i = 0; i<grid_squares_.Count; i++)
-        //{
-        //    grid_squares_[i].GetComponent<Grid>().SetNum(data.unsolved[i]);
-        //}
+        int gridRemove = 30; // Số ô cần xóa
+        List<List<int>> modifiedMatrix = RemoveRandomItems(data.matrix, gridRemove);
+        
+        //gán giá trị lấy từ SudokuData vào từng obj
         int k = 0;
         for (int i = 0; i < 9; i += 1)
         {
             for (int j = 0; j < 9; j += 1)
             {
-                grid_squares_[k].GetComponent<Grid>().SetNum(data.matrix[i][j]);
+                grid_squares_[k].GetComponent<Grid>().SetNum(modifiedMatrix[i][j]);//Gán lần lượt từng giá trị vào từng ô tương ứng trong grid_squares_
                 k += 1;
             }
         }
     }
+
+    // hiển thị giá trị của ô đã chọn
+    public void DisplaySelectedValue(int value)
+    {
+        selectedValue = value;
+    }
+
 }
